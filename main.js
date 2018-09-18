9c906c9b82cbd138500f4cebd5d050808df9be98
Maciej Kućmierowski <35165148+maciekKuc@users.noreply.github.com>
2018-09-18 21:45:28 +0200

    V0.002
    some jquery events

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 $(document).ready(() =>{
-	$('film').on('click', () => {
-		$('li').show();
+	$('.film').on('click', () => {
+		$('.filmMenu li').fadeToggle();
+	});
+	$('.serial').on('click', () => {
+		$('.showMenu li').fadeToggle();
+	});
+	$('.gra').on('click', () => {
+		$('.gameMenu li').fadeToggle();
 	});
 });
