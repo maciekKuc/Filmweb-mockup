7715ec94dc8429feff8db0106b51cc0ff01b3028
Maciej Kućmierowski <35165148+maciekKuc@users.noreply.github.com>
2018-09-26 21:28:14 +0200

    update the forEach

--- main.js
+++ main.js
@@ -27,6 +27,56 @@ $(document).ready(() =>{
 		}
 		]
 	};
+	let ratings = {
+		star: '',
+		actory: [{
+			name: 'Christoph Waltz',
+			pic: 'https://1.fwcdn.pl/ph/05/41/620541/400388_5.2.jpg',
+			rating: '9,4',
+			votes: '10625'
+		},
+		{
+			name: 'Leonardo DiCaprio',
+			pic: 'https://1.fwcdn.pl/ph/05/41/620541/361695_2.2.jpg',
+			rating: '9,2',
+			votes: '13282'
+		},
+		{
+			name: 'Samuel L. Jackson',
+			pic: 'https://1.fwcdn.pl/ph/05/41/620541/400387_1.2.jpg',
+			rating: '9,1',
+			votes: '7398'
+		},
+		{
+			name: 'Jamie Foxx',
+			pic: 'https://1.fwcdn.pl/ph/05/41/620541/400383_2.2.jpg',
+			rating: '8,6',
+			votes: '6359'
+		},
+		{
+			name: 'Kerry Washington',
+			pic: 'https://1.fwcdn.pl/ph/05/41/620541/361693_2.2.jpg',
+			rating: '7,5',
+			votes: '2577'
+		}
+		],
+		moveRight: () => {
+
+		},
+		moveLeft: () => {
+
+		},
+		createPanel: (obj) => {
+			let html = '';
+			this.actory.forEach((actor) => {
+				html += `<div>
+							<div style='bacground:url('${actor.pic}; width: 185px; height:185px')'><p>${actor.name}</p></div>
+							<div><p>${actor.rating}<span>${actor.votes} głosów</span></p></div>
+						</div>`;
+			});
+			obj.appendChild(html);
+		}
+	}
 	let setCast = (obj) =>{
 		let personelCont = document.getElementById('personelList');
 		personelCont.innerHTML='';
@@ -45,6 +95,10 @@ $(document).ready(() =>{
 		$(event.currentTarget).addClass('button-yellow');
 		$('#staff').removeClass('button-yellow');
 	});
+
+	let rateActors = document.getElementById('leadRoles');
+	ratings.createPanel(rateActors);
+
 	$('.film').on('mouseenter', () => {
 		$('.filmMenu li').show();
 	});
