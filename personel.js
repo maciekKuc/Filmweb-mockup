b2614d5b52eb1f5afdf93e5d7e9592d492dceaa1
Maciej Kućmierowski <35165148+maciekKuc@users.noreply.github.com>
2018-10-28 19:35:07 +0100

    Update personel.js

--- personel.js
+++ personel.js
@@ -12,34 +12,34 @@ let personel = {
 			rolePic: 'https://1.fwcdn.pl/ph/05/41/620541/400388_5.0.jpg'
 		},
 		{
-			picture: 'https://1.fwcdn.pl/p/95/01/69501/358591.2.jpg',
-			name: 'Christoph Waltz',
-			role: 'Dr King Schultz',
-			rolePic: 'https://1.fwcdn.pl/ph/05/41/620541/400388_5.0.jpg'
+			picture: 'https://1.fwcdn.pl/p/00/30/30/398646.2.jpg',
+			name: 'Leonardo DiCaprio',
+			role: 'Calvin Candle',
+			rolePic: 'https://1.fwcdn.pl/ph/05/41/620541/361695_2.0.jpg'
 		},
 		{
-			picture: 'https://1.fwcdn.pl/p/95/01/69501/358591.2.jpg',
-			name: 'Christoph Waltz',
-			role: 'Dr King Schultz',
-			rolePic: 'https://1.fwcdn.pl/ph/05/41/620541/400388_5.0.jpg'
+			picture: 'https://1.fwcdn.pl/p/00/58/40058/317838.2.jpg',
+			name: 'Kerry Washington',
+			role: 'Broomhilda von Schaft',
+			rolePic: 'https://1.fwcdn.pl/ph/05/41/620541/361693_2.0.jpg'
 		},
 		{
-			picture: 'https://1.fwcdn.pl/p/95/01/69501/358591.2.jpg',
-			name: 'Christoph Waltz',
-			role: 'Dr King Schultz',
-			rolePic: 'https://1.fwcdn.pl/ph/05/41/620541/400388_5.0.jpg'
+			picture: 'https://1.fwcdn.pl/p/00/53/53/346677.2.jpg',
+			name: 'Samuel L. Jackson',
+			role: 'Stephen',
+			rolePic: 'https://1.fwcdn.pl/ph/05/41/620541/400387_1.0.jpg'
 		},
 		{
-			picture: 'https://1.fwcdn.pl/p/95/01/69501/358591.2.jpg',
-			name: 'Christoph Waltz',
-			role: 'Dr King Schultz',
-			rolePic: 'https://1.fwcdn.pl/ph/05/41/620541/400388_5.0.jpg'
+			picture: 'https://1.fwcdn.pl/p/58/34/515834/377845.2.jpg',
+			name: 'Walton Goggins',
+			role: 'Billy Crash',
+			rolePic: 'https://1.fwcdn.pl/ph/05/41/620541/400389_3.0.jpg'
 		},
 		{
-			picture: 'https://1.fwcdn.pl/p/95/01/69501/358591.2.jpg',
-			name: 'Christoph Waltz',
-			role: 'Dr King Schultz',
-			rolePic: 'https://1.fwcdn.pl/ph/05/41/620541/400388_5.0.jpg'
+			picture: 'https://1.fwcdn.pl/p/37/71/3771/317204.2.jpg',
+			name: 'James Remar',
+			role: 'Butch Pooch / Ace Speck',
+			rolePic: ''
 		}
 		],
 		staff: [{
@@ -51,7 +51,25 @@ let personel = {
 		{
 			picture: 'https://1.fwcdn.pl/p/01/11/111/386849.2.jpg',
 			name: 'Quentin Tarantino',
-			role: ' scenariusz',
+			role: 'scenariusz',
+			rolePic: ''
+		},
+		{
+			picture: 'https://1.fwcdn.pl/p/03/95/10395/341431.2.jpg',
+			name: 'Robert Richardson',
+			role: 'zdjęcia',
+			rolePic: ''
+		},
+		{
+			picture: 'https://1.fwcdn.pl/p/00/21/330021/377820.2.jpg',
+			name: 'Fred Raskin',
+			role: 'montaż',
+			rolePic: ''
+		},
+		{
+			picture: 'https://1.fwcdn.pl/p/26/41/12641/346106.2.jpg',
+			name: 'Leslie A. Pope',
+			role: 'scenografia (dekorator wnętrz)',
 			rolePic: ''
 		}
 		]
